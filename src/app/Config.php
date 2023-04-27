5a03fe8fdfad5d291d7e4c2d7ecd5fb9abfc8f28
Kyle Hopkins <kyle@kyleweb.dev>
2023-04-26 18:33:33 -0600

    Rearrange flordia_land project tree after moving files to new git repo

---
+++ src/app/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+class Config {
+   /**
+    * path to the sqlite file
+    */
+    const PATH_TO_SQLITE_FILE = '/mnt/common_drive/common_home/coding/florida_land/db/sqlite.db';
+}
